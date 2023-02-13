b1a4a701ecb147aceb0f5c41cc012178943b4d42
rashurani09 <124286997+rashurani09@users.noreply.github.com>
2023-02-13 19:48:06 +0530

    Add files via upload
    •	Developed a web application which can be used to listen musics
    •	Users can play music based on their choice
    •	It is digital music application that gives you access to millions of songs from all over the world
    •	Playing music are totally free
    •	Technology used Html + css

---
+++ spotify/songs/hdjhdj.py
@@ -0,0 +1,11 @@
+import csv
+ 
+# opening the CSV file
+with open('IRIS.csv', mode ='r')as file:
+   
+  # reading the CSV file
+  csvFile = csv.reader(file)
+ 
+  # displaying the contents of the CSV file
+  for lines in csvFile:
+        print(lines)
